2055e5d5abde3dacb7806479582aa3ab22f2d301
Ian Wehba <56119986+ian-double-u@users.noreply.github.com>
2020-07-11 14:50:14 -0700

    Create euler3.py

---
+++ euler3.py
@@ -0,0 +1,23 @@
+def biggest_factor(x):
+    pf = []
+    
+    while x % 2 == 0:
+        pf.append(x)
+        x /= 2
+
+    y = 3
+
+    while y*y <= x:
+        if x % y == 0:
+            pf.append(y)
+            x /= y
+
+        else:
+            y += 2
+
+    if x != 1:
+        pf.append(x)
+        
+    return max(pf)
+
+print(f'Ans = {int(biggest_factor(600851475143))}')
