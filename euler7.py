5d85a5a18d0b894fb95c24d3ce2378cf53b04a05
Ian Wehba <56119986+ian-double-u@users.noreply.github.com>
2020-07-10 21:05:58 -0700

    Create euler7.py

---
+++ euler7.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+n = 110000
+A = [True]*n
+    
+for i in range(2,int(np.sqrt(n))+1): # Sieve of Eratosthenes
+    if A[i] == True:
+        j = i**2
+        while j < n:
+            A[j] = False
+            j += i
+
+counter = []
+for z in range(0,len(A)):
+    if A[z] == True:
+        counter.append(z)
+    
+print(counter[10002]) # counts 0 and 1 for some reason
