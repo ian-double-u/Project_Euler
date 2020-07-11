932c61c78c439ee8ede513d5ce0bdbbeba0e952e
Ian Wehba <56119986+ian-double-u@users.noreply.github.com>
2020-07-11 11:53:40 -0700

    Create euler2.py

---
+++ euler2.py
@@ -0,0 +1,17 @@
+fib = [1,2]
+
+def term(a,b): 
+    f = a + b
+    return f
+
+while fib[-1] < 4000000:
+    fib.append(term(fib[-2],fib[-1]))
+    
+fib.remove(fib[-1])
+
+sum = 0
+for f in fib:
+    if f%2 == 0:
+        sum += f
+        
+print(f'Ans = {sum}')
