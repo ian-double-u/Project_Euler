20ab97918d0dee9baa2c12ca8e06b74e054eded2
Ian Wehba <56119986+ian-double-u@users.noreply.github.com>
2020-08-07 12:27:44 -0700

    Update euler9.py

--- euler9.py
+++ euler9.py
@@ -1,6 +1,6 @@
 for i in range(1,1000): # i = a
     for j in range(1,1000): # j = b
-        for k in range(1,1000): # c = k
+        for k in range(1,1000): # k = c
             if (i+j+k == 1000):
                 if (i**2 + j**2 == k**2):
                     print(f'Ans = {i*j*k}')
