482869f9abc3dcfc8521c10477f2eb9385de764f
Ian Wehba <56119986+ian-double-u@users.noreply.github.com>
2020-08-13 18:00:42 -0700

    Update euler1.py

--- euler1.py
+++ euler1.py
@@ -1,8 +1,1 @@
-multiples = []
-for i in range(1,1000):
-    if i % 3 == 0:
-        multiples.append(i)
-    elif i % 5 == 0:
-        multiples.append(i)
-        
-print(f'Ans = {sum(multiples)}')
+print(f'Ans = {sum([i for i in range(1,1000) if i % 3 == 0 or i % 5 == 0])}')
